998d7ce2d8bd9132b9485e75bba7777b1dd04c32
Julio Valiente <juliorvaliente@gmail.com>
2022-09-26 11:36:20 -0600

    l

--- members.py
+++ members.py
@@ -1,7 +1,7 @@
 Khris_msg = "AYO whats good!"
 amer_msg = "Hell from amer"
 johann_msg = "Hello"
-Julio_msg = "watashi wa Julio desu"
+Julio_msg = "watashi wa Julio desu new change"
 print(Khris_msg)
 print(Julio_msg)
 print(amer_msg)
